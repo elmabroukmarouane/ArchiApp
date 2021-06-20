c50ddcdd1850466d09a27705d1029fe80923936b
Marouane EL MABROUK <elmabroukmarouane@gmail.com>
2021-06-20 17:35:10 +0200

    Create IBaseModel and BaseModel

---
+++ app/Infrastructure/IBaseModel.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Infrastucture;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+
+interface IBaseModel
+{
+    /**
+     * Get all models.
+     *
+     * @param array $columns
+     * @param array $relations
+     * @return Collection
+     */
+    public function all(array $columns = ['*'], array $relations = []): Collection;
+
+    /**
+     * Get all trashed models.
+     *
+     * @return Collection
+     */
+    public function allTrashed(): Collection;
+
+    /**
+     * Find model by id.
+     *
+     * @param int $modelId
+     * @param array $columns
+     * @param array $relations
+     * @param array $appends
+     * @return Model
+     */
+    public function findById(
+        int $modelId,
+        array $columns = ['*'],
+        array $relations = [],
+        array $appends = []
+    ): ?Model;
+
+    /**
+     * Find trashed model by id.
+     *
+     * @param int $modelId
+     * @return Model
+     */
+    public function findTrashedById(int $modelId): ?Model;
+
+    /**
+     * Find only trashed model by id.
+     *
+     * @param int $modelId
+     * @return Model
+     */
+    public function findOnlyTrashedById(int $modelId): ?Model;
+
+    /**
+     * Select by SQL Query
+     *
+     * @param $query
+     * @return Model
+    */
+    public function selectByQuery($query): array;
+
+    /**
+     * Create a model.
+     *
+     * @param array $payload
+     * @return Model
+     */
+    public function create(array $payload): ?Model;
+
+    /**
+     * Update existing model.
+     *
+     * @param int $modelId
+     * @param array $payload
+     * @return bool
+     */
+    public function update(int $modelId, array $payload): bool;
+
+    /**
+     * Delete model by id.
+     *
+     * @param int $modelId
+     * @return bool
+     */
+    public function deleteById(int $modelId): bool;
+
+    /**
+     * Restore model by id.
+     *
+     * @param int $modelId
+     * @return bool
+     */
+    public function restoreById(int $modelId): bool;
+
+    /**
+     * Permanently delete model by id.
+     *
+     * @param int $modelId
+     * @return bool
+     */
+    public function permanentlyDeleteById(int $modelId): bool;
+}
